3d03a71681f20841d4ec8a9a1dc12f27637dd9c4
Evandro Harrison Hoffmann <ehh85@yahoo.com.br>
2016-12-16 14:09:29 +0800

    Tweeks Landscape mode to work with 3D touch.

--- MVMedia/Classes/Controllers/MVMediaLandscapeNavigationController.swift
+++ MVMedia/Classes/Controllers/MVMediaLandscapeNavigationController.swift
@@ -12,19 +12,29 @@ open class MVMediaLandscapeNavigationController: UINavigationController {
 
     override open func viewDidLoad() {
         super.viewDidLoad()
-        MVMediaManager.shared.isPlayingLandscapeMedia = true
     }
     
     override open func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        becomePortrait()
+    }
+    
+    override open func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        resignPortrait()
+    }
+    
+    open func becomePortrait(){
+        MVMediaManager.shared.isPlayingLandscapeMedia = true
         
         //to make sure it will rotate, trick it saying that it was in portrait before
         UIDevice.current.setValue(Int(UIInterfaceOrientation.portrait.rawValue), forKey: "orientation")
         UIDevice.current.setValue(Int(UIInterfaceOrientation.landscapeRight.rawValue), forKey: "orientation")
     }
     
-    override open func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    open func resignPortrait() {
         MVMediaManager.shared.isPlayingLandscapeMedia = false
         
         //to make sure it will rotate, trick it saying that it was in landscape before
